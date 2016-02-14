b300ceef4e5437945cde9da02d203d3058fd5223
Fabian Wenzelmann <wenzelmf@informatik.uni-freiburg.de>
2016-02-14 15:39:56 +0100

    django skeleton

---
+++ django_bootstrap3_datepicker/django_bootstrap3_datepicker/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for django_bootstrap3_datepicker project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_bootstrap3_datepicker.settings")
+
+application = get_wsgi_application()
